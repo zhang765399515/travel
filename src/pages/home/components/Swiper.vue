be32e6f93ced603fb44592253b839acf015f85a0
zhang765399515 <765399515@qq.com>
2019-03-01 17:35:00 +0800

    轮播

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList">
+        <img
+          class="swiper-img"
+          :src="item.imgUrl"
+          alt
+        >
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      },
+      swiperList:[{
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/e3/62ce7362ca051d02.jpg_640x200_6db551b7.jpg'
+      },{
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/e3/62ce7362ca051d02.jpg_640x200_6db551b7.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+.wrapper
+  overflow:hidden
+  width:100%
+  height:0
+  padding-bottom:31.25%
+  background:#eee
+  .swiper-img
+    width: 100%
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,14 +1,17 @@
 <template>
 <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
+    <div></div>
 </div>
 </template>
 <script>
 import HomeHeader from './components/header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader, HomeSwiper
   }
 }
 </script>
